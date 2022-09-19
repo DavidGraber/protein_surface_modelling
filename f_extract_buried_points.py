86e4999aa9dd3440b0d1950dc02d1d4afa58dc2d
DavidGraber <david.graber@bluewin.ch>
2022-09-19 18:29:31 +0200

    created function py files and main.py

---
+++ f_extract_buried_points.py
@@ -0,0 +1,23 @@
+def extract_buried_points(subunit_coords, complex_coords, threshold):
+
+    '''Function that takes as input the coordinates of two similar pointclouds (one subunit and
+    one complex involving that subunit) and a threshold. Returns a list of indeces indicating 
+    which points of the first pointcloud (subunit) are more than a certain distance (threshold)
+    away from any point in the second pointcloud. These points are "buried" in the complex. The 
+    point with the largest distance above the threshold is returned as the "center" of the buried patch'''
+
+    from sklearn.neighbors import NearestNeighbors
+    
+    neigh = NearestNeighbors(n_neighbors=1)
+    neigh.fit(complex_coords) 
+
+    buried= []
+    largest_dist = 0
+    for i in range(len(subunit_coords)):
+        if (dist := neigh.kneighbors([subunit_coords[i]])[0][0][0]) > threshold:
+        #if neigh.kneighbors([subunit_coords[i]])[0][0][0] > threshold:
+            buried.append(i)
+            if dist > largest_dist:
+                largest_dist = dist
+                center = i
+    return buried, center
---
+++ f_extract_surface_patch.py
@@ -0,0 +1,177 @@
+def generate_graph(indeces, coords_sel, normals):
+
+    '''Function that takes a set of points, with their label, coordinates and surface normals. Calculates for each point the 
+    geodesic distance to its n nearest neighbors and saves that information in a dictionary representing a graph. '''
+    
+    from sklearn.neighbors import NearestNeighbors
+    import numpy as np
+
+    knn = NearestNeighbors(n_neighbors=4)
+    knn.fit(coords_sel)
+    graph = {}
+    
+    #loop through each point that is within the radius and find its nearest neighbors and their euclidean distance
+    for idx, point in enumerate(coords_sel):
+        dist, neighbors = knn.kneighbors([point], return_distance=True)
+        
+        # loop through the nearest neighbors calculate their geodesic distance to the point chosen above
+        # Add the geodesic distance to a graph-dictionary
+        connected_nodes = []
+        for index, neighbor in enumerate(neighbors[0]):
+            geo_dist = dist[0][index]*(2-np.dot(normals[indeces[idx]], normals[indeces[neighbor]]))        
+            #distances.at[ first_sel[idx] , first_sel[neighbor] ] = geo_dist
+            if geo_dist !=0:
+                connected_nodes.append((indeces[neighbor], geo_dist))
+
+        graph[indeces[idx]]=connected_nodes                      
+    
+    return graph
+
+
+def make_graph_bidirectional(graph):  
+
+    '''The result of the generate_graph function is a graph where individual points can be "connected" to another point, but not
+    vice-versa. This function detect unidirectional connections and adds the connection information to both involved points'''
+    
+    #loop through all the keys
+    for key in graph:
+        
+        # loop through all the neighbors of the key and their distances to the key
+        for p, d in graph[key]:
+            # For each neighbor, set bidirectional = False
+            bidirectional = False
+            
+            # check the graph entry of that neighbor, if there the key is among its connected points, set bidire = True
+            for pt,_ in graph[p]:
+                if pt == key:
+                    bidirectional = True
+
+            if not bidirectional:
+                current_list = graph[p]
+                current_list.append((key,d))
+                graph[p]=current_list
+                
+    return graph
+
+
+def dijkstra(graph, center):
+    
+    '''Function that takes a graph and the starting node and returns a list of distances 
+    from the starting node to every other node'''
+
+    n = len(graph) # How many nodes are in the graph?
+    
+    # initialize a dictionary to save the distances of each node from the start node
+    dist_from_center = {}
+    for key in graph:
+        dist_from_center[key]=1000
+        
+    # initialize a dictionary to save which node has been visited already
+    visited={}
+    for key in graph:
+        visited[key]=False
+            
+    # set the distance for the start to be 0
+    dist_from_center[center] = 0
+    
+    for p in range(n): 
+        # loop through all the nodes to check which one is not yet visited and has the lowest distance to the current node
+        u = -1
+        for key in graph:
+            # if the node 'key' hasn't been visited and
+            # we haven't processed it or the distance we have for it is less
+            # than the distance we have to the "start" node
+            
+            # our start node (4557) will be selected first and assigned to u
+            if not visited[key] and (u == -1 or dist_from_center[key] < dist_from_center[u]):
+                u = key 
+        
+        
+        # all the nodes have been visited or we can't reach this node
+        if dist_from_center[u] == 1000:
+            #print("break")
+            break
+        
+        # set the node as visited
+        visited[u] = True
+        
+        # from the current selected node u, check what the distances to the next nodes are and update their dist from center
+        # loop through all the points (and their weights) that can be reached from our current node
+        for v, l in graph[u]:
+            if dist_from_center[u] + l < dist_from_center[v]:
+                dist_from_center[v] = dist_from_center[u] + l
+    return dist_from_center
+
+
+
+def extract_surface_patch(coords, center_index, radius):
+    
+    import open3d as o3d
+    import numpy as np
+    import pandas as pd
+
+    pointcloud = o3d.geometry.PointCloud()
+    pointcloud.points = o3d.utility.Vector3dVector(coords)
+    pointcloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn = 5))
+    pointcloud.orient_normals_consistent_tangent_plane(k=5)
+    normals = np.asarray(pointcloud.normals)
+
+    #Select a random point of the cloud, around which to draw a geodesic circle, set a geodesic radius
+    radius = radius
+    center_index = center_index
+    center_coords = coords[center_index]
+    center_norm = normals[center_index]
+
+    first_sel = [center_index] # to save all the points that are within the non-geodesic radius
+
+    #loop through all the points and calculate their euclidean distance to the selected center
+    for index, point in enumerate(coords):
+        dist = np.linalg.norm(coords[center_index]-point)
+
+        # first selection with only those points that are close to the center point
+        if dist < radius and dist != 0:
+            first_sel.append(index)
+            
+    coords_sel = coords[first_sel]
+
+    # generate a graph with the selected points
+    graph = generate_graph(first_sel, coords_sel, normals)
+
+    # make graph bidirectional
+    graph = make_graph_bidirectional(graph)
+
+    # check for each point the GEODESIC distance to the center with djikstra
+    dist_from_center = dijkstra(graph, center_index)
+
+
+    # Collect the indeces of the points that are < radius away from the center point
+    patch_indeces = []
+
+    for key in dist_from_center:
+        if dist_from_center[key]<=radius:
+            patch_indeces.append(key)
+            
+    patch_coords = coords[patch_indeces]
+    patch_normals = normals[patch_indeces]
+
+    # Make a graph out of the extracted patch
+    patch_graph = generate_graph(patch_indeces, patch_coords, normals)
+    patch_graph = make_graph_bidirectional(patch_graph)
+
+    # Generate a double dictionary where the distance between two points can be accessed with dict[point1][point2]
+    pairwise_dist_dict = {}
+    for key in patch_graph:
+        distances = dijkstra(patch_graph, key)
+        pairwise_dist_dict[key]=distances
+
+    # Generate a quadratic dataframe for the pairwise distances between all points, label the columns and rows accordingly
+    pairwise_distances = pd.DataFrame(np.zeros((len(patch_indeces),len(patch_indeces))))
+    pairwise_distances.columns = patch_indeces
+    pairwise_distances.index = patch_indeces
+
+    # Add the distance information stored in the pairwise_dist_dict to the dataframe
+    for index in patch_indeces:
+        for idx in patch_indeces:
+            pairwise_distances.at[index, idx] = pairwise_dist_dict[index][idx]
+
+    return patch_indeces, patch_coords, pairwise_distances
