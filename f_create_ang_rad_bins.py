6619041ef8f78aeef5a4952c2e63cc2a158033d5
DavidGraber <david.graber@bluewin.ch>
2022-09-29 16:53:25 +0200

    Improvement in NN calculation

--- f_create_ang_rad_bins.py
+++ f_create_ang_rad_bins.py
@@ -6,39 +6,32 @@ from f_useful_functions import polar_to_cart
 
 def create_ang_rad_bins(
     n_angular_bins,
-    n_radial_bins, 
+    n_radial_bins,
+    max_rho,
     features_patch,
-    polar_coords_patch,
     cart_coords_patch,
     n_nearest_neigh
     ):
 
-    max_rho = np.max(polar_coords_patch[:,0])
+    #max_rho = np.max(polar_coords_patch[:,0])
 
-    # Create meshgrid n_angular_bins x n_radial_bins
-    angular, radial = np.mgrid[-np.pi:np.pi:complex(n_angular_bins), 0:max_rho:complex(n_radial_bins)]
+    # Create meshgrid n_angular_bins x n_radial_bins (+1 to have the right numb or spaces in between)
+    angular, radial = np.mgrid[-np.pi:np.pi:complex(n_angular_bins+1), 0:max_rho:complex(n_radial_bins+1)]
     
     # Create empty feature array with one n_angular_bins x n_radial_bins x n_features
-    z = np.zeros((angular.shape[0], angular.shape[1], features_patch.shape[1]))
-
-    # Normalize the features (IF POSSIBLE DO THIS SOMEWHERE ELSE)
-    #features_patch_n = normalize_columns_01(features_patch)
-
-    # normalize the inputs for the kNN for it to work properly
-    #angular_n = normalize_01(angular)
-    #radial_n = normalize_01(radial)
-    #polar_coords_patch_n = normalize_columns_01(polar_coords_patch)
+    z = np.zeros((angular.shape[0]-1, angular.shape[1]-1, features_patch.shape[1]))
 
     knn = NearestNeighbors(n_neighbors=n_nearest_neigh)
     knn.fit(cart_coords_patch)
 
     # Loop through each of the bins
     for angular_bin in range(n_angular_bins):
+        
+        theta = np.mean([angular[angular_bin,0], angular[angular_bin+1, 0]])
+
         for radial_bin in range(n_radial_bins):
             
-            # The polar coordinates of the bin rho=radial, theta=angular
-            theta = angular[angular_bin,0]
-            rho = radial[0, radial_bin]
+            rho = np.mean([radial[0, radial_bin], radial[0, radial_bin+1]])
 
             # Convert to cartesian coordinates and look for nearest neighbors in cartesian coords
             cart = polar_to_cart(np.array([rho, theta]))
